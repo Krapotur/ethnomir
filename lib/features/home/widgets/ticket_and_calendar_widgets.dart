e2dc5ff4112245d115788aea3f089b132759972a
Krapotur <dayfive2220@gmail.com>
2025-12-12 13:07:37 +0300

    Add Inkwell button

--- lib/features/home/widgets/ticket_and_calendar_widgets.dart
+++ lib/features/home/widgets/ticket_and_calendar_widgets.dart
@@ -9,68 +9,74 @@ class TicketAndCalendarWidgets extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        CardWidget(
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                height: 55,
-                width: 55,
-                margin: EdgeInsets.only(
-                  left: 5,
-                  top: 10,
-                  bottom: 10,
-                  right: 10,
-                ),
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 255, 178, 176),
-                  borderRadius: BorderRadius.circular(10),
-                  image: DecorationImage(
-                    image: AssetImage("assets/images/ticket.png"),
-                    fit: BoxFit.cover,
+        InkWell(
+          onTap: () {},
+          child: CardWidget(
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  height: 55,
+                  width: 55,
+                  margin: EdgeInsets.only(
+                    left: 5,
+                    top: 10,
+                    bottom: 10,
+                    right: 10,
+                  ),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 255, 178, 176),
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(
+                      image: AssetImage("assets/images/ticket.png"),
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
-              ),
-              Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [Text('Билет'), Text('в ЭтноМир')],
-              ),
-            ],
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [Text('Билет'), Text('в ЭтноМир')],
+                ),
+              ],
+            ),
           ),
         ),
         SizedBox(width: 10),
-        CardWidget(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                height: 55,
-                width: 55,
-                clipBehavior: Clip.hardEdge,
-                margin: EdgeInsets.only(
-                  left: 5,
-                  top: 10,
-                  bottom: 10,
-                  right: 10,
-                ),
-                decoration: BoxDecoration(
-                  color: const Color(0xFFFFBAB6),
-                  borderRadius: BorderRadius.circular(10),
-                  image: DecorationImage(
-                    image: AssetImage("assets/images/calendar.png"),
-                    fit: BoxFit.contain,
+        InkWell(
+          onTap: () {},
+          child: CardWidget(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  height: 55,
+                  width: 55,
+                  clipBehavior: Clip.hardEdge,
+                  margin: EdgeInsets.only(
+                    left: 5,
+                    top: 10,
+                    bottom: 10,
+                    right: 10,
+                  ),
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFFFBAB6),
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(
+                      image: AssetImage("assets/images/calendar.png"),
+                      fit: BoxFit.contain,
+                    ),
                   ),
                 ),
-              ),
 
-              Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [Text('Календарь'), Text('событий')],
-              ),
-            ],
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [Text('Календарь'), Text('событий')],
+                ),
+              ],
+            ),
           ),
         ),
       ],
